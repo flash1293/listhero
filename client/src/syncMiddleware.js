494bfdfd268602d9357a7470c50eea3354436779
Johannes Reuter <jreuter@inovex.de>
2017-12-06 18:10:49 +0100

    rename function in  middleware

--- client/src/syncMiddleware.js
+++ client/src/syncMiddleware.js
@@ -1,98 +1,97 @@
 import { isNumber } from "util";
 
-
 export default (postAction, filter, key) => {
-    let syncLog;
-    const saveSyncLog = () => {
-        localStorage.setItem(`sync-log:${key}`, JSON.stringify(syncLog));
-    };
-    const loadSyncLog = () => {
-        const serializedLog = localStorage.getItem(`sync-log:${key}`);
-        if (serializedLog) {
-            syncLog = JSON.parse(serializedLog);
-        } else {
-            syncLog = [];
-        }
-    };
-    const isSeedCorrect = (seed) => {
-        const storageKey = `sync-seed:${key}`;
-        const savedSeed = localStorage.getItem(storageKey);
-        if (savedSeed) {
-            const isCorrect = seed === savedSeed;
-            if (!isCorrect) {
-                localStorage.setItem(storageKey, seed);
-            }
-            return isCorrect;
-        }
-        localStorage.setItem(storageKey, seed);
-        return true;
+  let syncLog;
+  const saveSyncLog = () => {
+    localStorage.setItem(`sync-log:${key}`, JSON.stringify(syncLog));
+  };
+  const loadSyncLog = () => {
+    const serializedLog = localStorage.getItem(`sync-log:${key}`);
+    if (serializedLog) {
+      syncLog = JSON.parse(serializedLog);
+    } else {
+      syncLog = [];
     }
-    let requestInFlight = false;
-    loadSyncLog();
-    return store => next => {
-        const getSyncState = () => store.getState()[key];
-        const startSync = () => {
-            if (requestInFlight) return;
-            console.log('attempting sync');
-            requestInFlight = true;
-            const actionsToSync = syncLog.splice(0, syncLog.length); 
-            postAction({
-                    startFrom: getSyncState().sequence - actionsToSync.length,
-                    actions: actionsToSync
-            })
-            .then(res => {
-                if(!isSeedCorrect(res.seed)) {
-                    console.log('purging local state, server has another seed');
-                    // server has another seed than the client, purge all data and resync
-                    next({
-                        type: '@@sync/PURGE',
-                        key
-                    });
-                    startSync();
-                } else if (isNumber(res.replayFrom)) {
-                    next({
-                        type: '@@sync/MERGE',
-                        key,
-                        undo: getSyncState().sequence - res.replayFrom,
-                        replayLog: res.replayLog.concat(syncLog)
-                    });
-                } else {
-                    next({
-                        type: '@@sync/SYNC',
-                        key
-                    });
-                }
-                if (syncLog.length > 0) {
-                    console.log('sync completed, restarting for new changes');
-                    saveSyncLog();
-                    startSync();
-                } else {
-                    console.log('sync completed');
-                    saveSyncLog();
-                    requestInFlight = false;
-                }
-            })
-            .catch(() => {
-                console.log('sync failed, retry in 1 second');
-                // sync failed, prepend actions which were selected to sync back to the synclog and try again
-                syncLog = actionsToSync.concat(syncLog);
-                requestInFlight = false;
-                setTimeout(startSync, 1000);
+  };
+  const checkAndUpdateSeed = seed => {
+    const storageKey = `sync-seed:${key}`;
+    const savedSeed = localStorage.getItem(storageKey);
+    if (savedSeed) {
+      const isCorrect = seed === savedSeed;
+      if (!isCorrect) {
+        localStorage.setItem(storageKey, seed);
+      }
+      return isCorrect;
+    }
+    localStorage.setItem(storageKey, seed);
+    return true;
+  };
+  let requestInFlight = false;
+  loadSyncLog();
+  return store => next => {
+    const getSyncState = () => store.getState()[key];
+    const startSync = () => {
+      if (requestInFlight) return;
+      console.log("attempting sync");
+      requestInFlight = true;
+      const actionsToSync = syncLog.splice(0, syncLog.length);
+      postAction({
+        startFrom: getSyncState().sequence - actionsToSync.length,
+        actions: actionsToSync
+      })
+        .then(res => {
+          if (!checkAndUpdateSeed(res.seed)) {
+            console.log("purging local state, server has another seed");
+            // server has another seed than the client, purge all data and resync
+            next({
+              type: "@@sync/PURGE",
+              key
             });
-        };
-        return action => {
-            const oldState = getSyncState();
-            const result = next(action);
-            if (action.type === '@@sync/REQUEST_SYNC' && action.key === key) {
-                startSync();
-            } else {
-                if (oldState !== getSyncState() && filter(action)) {
-                    syncLog.push(action);
-                    saveSyncLog();
-                    startSync();
-                }
-            }
-            return result;
-        }
+            startSync();
+          } else if (isNumber(res.replayFrom)) {
+            next({
+              type: "@@sync/MERGE",
+              key,
+              undo: getSyncState().sequence - res.replayFrom,
+              replayLog: res.replayLog.concat(syncLog)
+            });
+          } else {
+            next({
+              type: "@@sync/SYNC",
+              key
+            });
+          }
+          if (syncLog.length > 0) {
+            console.log("sync completed, restarting for new changes");
+            saveSyncLog();
+            startSync();
+          } else {
+            console.log("sync completed");
+            saveSyncLog();
+            requestInFlight = false;
+          }
+        })
+        .catch(() => {
+          console.log("sync failed, retry in 1 second");
+          // sync failed, prepend actions which were selected to sync back to the synclog and try again
+          syncLog = actionsToSync.concat(syncLog);
+          requestInFlight = false;
+          setTimeout(startSync, 1000);
+        });
     };
-}
+    return action => {
+      const oldState = getSyncState();
+      const result = next(action);
+      if (action.type === "@@sync/REQUEST_SYNC" && action.key === key) {
+        startSync();
+      } else {
+        if (oldState !== getSyncState() && filter(action)) {
+          syncLog.push(action);
+          saveSyncLog();
+          startSync();
+        }
+      }
+      return result;
+    };
+  };
+};
