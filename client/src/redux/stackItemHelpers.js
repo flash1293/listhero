3afecde0fe23af5c9c2407251c591c5491b55fb0
Johannes Reuter <jreuter@inovex.de>
2017-12-19 17:16:48 +0100

    refactor stack-helpers

--- client/src/redux/stackItemHelpers.js
+++ client/src/redux/stackItemHelpers.js
@@ -1,22 +1,31 @@
+import useWith from "ramda/src/useWith";
+import identity from "ramda/src/identity";
+
 const stackRegex = /(\d+) (.+)/;
-export const stackItemIndex = (list, item) =>
-  list.findIndex(
-    i =>
-      i.name === item ||
-      (stackRegex.test(i.name) && stackRegex.exec(i.name)[2] === item)
-  );
+
+const parseStackedItem = item => {
+  const result = stackRegex.exec(item);
+  return {
+    item,
+    isStacked: !!result,
+    count: result ? parseInt(result[1], 10) : 0,
+    label: result ? result[2] : ""
+  };
+};
+
 export const isStacked = item => stackRegex.test(item);
-export const increaseStack = item =>
-  isStacked(item)
-    ? `${parseInt(stackRegex.exec(item)[1], 10) + 1} ${
-        stackRegex.exec(item)[2]
-      }`
-    : `2 ${item}`;
-export const decreaseStack = item =>
-  isStacked(item)
-    ? parseInt(stackRegex.exec(item)[1], 10) > 2
-      ? `${parseInt(stackRegex.exec(item)[1], 10) - 1} ${
-          stackRegex.exec(item)[2]
-        }`
-      : stackRegex.exec(item)[2]
-    : item;
+export const stackItemIndex = useWith(
+  (list, { item, isStacked, label }) =>
+    list.findIndex(i => i.name === item || (isStacked && label === item)),
+  [identity, parseStackedItem]
+);
+export const increaseStack = useWith(
+  ({ item, isStacked, count, label }) =>
+    isStacked ? `${count + 1} ${label}` : `2 ${item}`,
+  [parseStackedItem]
+);
+export const decreaseStack = useWith(
+  ({ item, isStacked, count, label }) =>
+    isStacked ? (count > 2 ? `${count - 1} ${label}` : label) : item,
+  [parseStackedItem]
+);
