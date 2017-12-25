4f95c51493f4782526d4f0a64a4979e42dd0e680
Johannes Reuter <jreuter@inovex.de>
2017-12-25 09:05:39 +0100

    dont load login and home page dynamically

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -6,30 +6,28 @@ import { PersistGate } from "redux-persist/es/integration/react";
 import { store, persistor } from "../redux";
 import preloader from "../components/Preloader";
 import ThemeProvider from "../components/ThemeProvider";
+import Lists from "./Lists";
+import Login from "./Login";
 
 const PreloadedRouter = preloader({
-  lists: () => import("./Lists"),
-  login: () => import("./Login"),
   listsEdit: () => import("./ListsEdit"),
   viewList: () => import("./ViewList"),
   editList: () => import("./EditList"),
   recentUsed: () => import("./RecentUsed"),
   categories: () => import("./Categories")
-})(
-  ({ lists, login, listsEdit, viewList, editList, recentUsed, categories }) => (
-    <Router>
-      <div>
-        <Route exact path="/" component={lists} />
-        <Route exact path="/login" component={login} />
-        <Route exact path="/edit" component={listsEdit} />
-        <Route exact path="/lists/:id" component={viewList} />
-        <Route exact path="/lists/:id/edit" component={editList} />
-        <Route exact path="/lists/:id/edit/last-used" component={recentUsed} />
-        <Route exact path="/lists/:id/edit/categories" component={categories} />
-      </div>
-    </Router>
-  )
-);
+})(({ listsEdit, viewList, editList, recentUsed, categories }) => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Lists} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/edit" component={listsEdit} />
+      <Route exact path="/lists/:id" component={viewList} />
+      <Route exact path="/lists/:id/edit" component={editList} />
+      <Route exact path="/lists/:id/edit/last-used" component={recentUsed} />
+      <Route exact path="/lists/:id/edit/categories" component={categories} />
+    </div>
+  </Router>
+));
 
 export default () => (
   <PersistGate persistor={persistor}>
