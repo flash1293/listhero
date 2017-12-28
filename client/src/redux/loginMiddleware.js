cbd250e4825033cb9d85a5f6acf6f4ffbc68dc6d
Johannes Reuter <jreuter@inovex.de>
2017-12-28 10:27:22 +0100

    add login middleware

---
+++ client/src/redux/loginMiddleware.js
@@ -0,0 +1,26 @@
+import { API_PROTOCOL, API_HOST } from "../config";
+
+const performLogin = (username, password) =>
+  fetch(`${API_PROTOCOL}//${API_HOST}/token`, {
+    method: "post",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ username, password })
+  }).then(res => res.json());
+
+export default store => next => action => {
+  const result = next(action);
+  if (action.type === "LOGIN") {
+    performLogin(action.username, action.password).then(token => {
+      next({ type: "LOGIN_SUCCESS", token: token });
+      next({
+        type: "@@sync/REQUEST_SYNC",
+        key: "lists",
+        skipRetry: true
+      });
+    });
+  }
+  return result;
+};
