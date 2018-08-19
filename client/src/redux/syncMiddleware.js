9e5bc9bbfa5dbd3fac154f23f1c0bf0e788da162
Johannes Reuter <jreuter@inovex.de>
2018-08-19 17:52:22 +0200

    remove aes-wasm

--- client/src/redux/syncMiddleware.js
+++ client/src/redux/syncMiddleware.js
@@ -1,5 +1,4 @@
 import aes from "aes-js";
-import { aes256 } from "aes-wasm";
 import compose from "ramda/src/compose";
 
 import {
@@ -26,24 +25,15 @@ const checkAndUpdateSeed = seed => {
   return true;
 };
 
-let aes256Instance;
-function getAesInstance() {
-  if (!aes256Instance) {
-    return aes256().then(instance => {
-      aes256Instance = instance;
-      return instance;
-    });
-  } else {
-    return Promise.resolve(aes256Instance);
-  }
-}
-
-const encrypt = (crypt, data, key) => {
+const encrypt = (data, key) => {
   const seed = getRandomData(128);
-  crypt.init(key, seed, "CTR");
+  const crypt = new aes.ModeOfOperation.ctr(
+    key,
+    new aes.Counter(seed)
+  );
   return `0x${aes.utils.hex.fromBytes(seed)};${compose(
     aes.utils.hex.fromBytes,
-    crypt.encrypt,
+    crypt.encrypt.bind(crypt),
     aes.utils.utf8.toBytes,
     escape
   )(data)}`;
@@ -57,71 +47,56 @@ function getSeed(seed) {
   }
 }
 
-const decrypt = (crypt, data, key) => {
+const decrypt = (data, key) => {
   const [seed, seededData] = data.split(";");
   const convertedSeed = seededData ? getSeed(seed) : 1;
-  if (Number.isInteger(convertedSeed)) {
-    // legacy action, decrypt with aes-js
-    const legacyCrypt = new aes.ModeOfOperation.ctr(
-      key,
-      new aes.Counter(convertedSeed)
-    );
-    return compose(
-      unescape,
-      aes.utils.utf8.fromBytes,
-      legacyCrypt.decrypt.bind(legacyCrypt),
-      aes.utils.hex.toBytes
-    )(seededData ? seededData : data);
-  } else {
-    // modern action, decrypt with aes-wasm
-    crypt.init(key, new Uint8Array(convertedSeed), "CTR");
-    return compose(
-      unescape,
-      aes.utils.utf8.fromBytes,
-      crypt.decrypt,
-      aes.utils.hex.toBytes
-    )(seededData ? seededData : data);
-  }
+  const crypt = new aes.ModeOfOperation.ctr(
+    key,
+    new aes.Counter(convertedSeed)
+  );
+  return compose(
+    unescape,
+    aes.utils.utf8.fromBytes,
+    crypt.decrypt.bind(crypt),
+    aes.utils.hex.toBytes
+  )(seededData ? seededData : data);
 };
 
 const postActionCreator = store => req =>
-  getAesInstance().then(crypt =>
-    fetch(`${API_PROTOCOL}//${API_HOST}/api`, {
-      method: "post",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-        "X-Sync-Session": clientSession,
-        Authorization: `Bearer ${store.getState().user.token}`
-      },
-      body: JSON.stringify({
-        ...req,
-        actions: req.actions.map(action =>
-          encrypt(
-            crypt,
-            JSON.stringify(action),
-            store.getState().user.encryptionKey
-          )
+  fetch(`${API_PROTOCOL}//${API_HOST}/api`, {
+    method: "post",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+      "X-Sync-Session": clientSession,
+      Authorization: `Bearer ${store.getState().user.token}`
+    },
+    body: JSON.stringify({
+      ...req,
+      actions: req.actions.map(action =>
+        encrypt(
+          JSON.stringify(action),
+          store.getState().user.encryptionKey
         )
-      })
+      )
     })
-      .then(res => {
-        if (res.status === 401) {
-          throw new Error("unauthorized");
-        }
-        return res.json();
-      })
-      .then(jsonRes => {
-        if (jsonRes.replayLog) {
-          jsonRes.replayLog = jsonRes.replayLog.map(action =>
-            JSON.parse(
-              decrypt(crypt, action, store.getState().user.encryptionKey)
-            )
-          );
-        }
-        return jsonRes;
-      })
-  );
+  })
+    .then(res => {
+      if (res.status === 401) {
+        throw new Error("unauthorized");
+      }
+      return res.json();
+    })
+    .then(jsonRes => {
+      if (jsonRes.replayLog) {
+        jsonRes.replayLog = jsonRes.replayLog.map(action =>
+          JSON.parse(
+            decrypt(action, store.getState().user.encryptionKey)
+          )
+        );
+      }
+      return jsonRes;
+    });
 
 const syncFilter = action => action.type !== "persist/REHYDRATE";
 
