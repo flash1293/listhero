644b9214371e2e942980e6825f1036a8f60c3c5e
Johannes Reuter <email@johannes-reuter.de>
2020-01-06 15:50:53 +0100

    introduce application level transaction

--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,7 @@ const trace = require("debug")("trace");
 const info = require("debug")("info");
 const error = require("debug")("error");
 const MongoClient = require("mongodb").MongoClient;
+var Mutex = require("async-mutex").Mutex;
 
 (async () => {
   const mongo = await MongoClient.connect(
@@ -28,6 +29,8 @@ const MongoClient = require("mongodb").MongoClient;
   app.use(cors());
 
   const clients = {};
+
+  const locks = {};
 
   const scryptParams = scrypt.paramsSync(0.2);
 
@@ -79,110 +82,117 @@ const MongoClient = require("mongodb").MongoClient;
     jwtExpress({ secret: process.env.SECRET }),
     async (req, res) => {
       const { startFrom, actions, snapshot, version } = req.body;
-      try {
-        const data = await db.collection("data").findOne(
-          { username: req.user.username },
-          {
-            projection: {
-              actions: { $slice: [startFrom, Math.pow(2, 30)] },
-              snapshot: 1
-            }
-          }
-        );
-        const actionCount = startFrom + data.actions.length;
-        const storedSnapshot = data.snapshot || {};
+      if (!locks[req.user.username]) {
+        locks[req.user.username] = new Mutex();
+      }
 
-        if (actions.length > 0) {
-          await db.collection("data").updateOne(
+      await locks[req.user.username].runExclusive(async () => {
+        try {
+          const data = await db.collection("data").findOne(
             { username: req.user.username },
             {
-              $push: {
-                actions: { $each: actions.map(action => String(action)) }
+              projection: {
+                actions: { $slice: [startFrom, Math.pow(2, 30)] },
+                snapshot: 1
               }
             }
           );
-        }
+          const actionCount = startFrom + data.actions.length;
+          const storedSnapshot = data.snapshot || {};
 
-        const newActions = data.actions.concat(actions);
-
-        const sequence = actionCount;
-        let storedSnapshotSequence = storedSnapshot.sequence
-          ? parseInt(storedSnapshot.sequence)
-          : 0;
-        let storedSnapshotVersion = storedSnapshot.version
-          ? parseInt(storedSnapshot.version)
-          : undefined;
-        trace(storedSnapshotSequence);
-        trace(storedSnapshotVersion);
-        trace(sequence);
-        trace(newActions);
-        if (snapshot) {
-          info(`Trying to store snapshot`);
-          if (
-            (storedSnapshotVersion === undefined ||
-              version >= storedSnapshotVersion) &&
-            startFrom === sequence
-          ) {
-            info(`Storing snapshot`);
-            const snapshotSequence = sequence + actions.length;
-            await db.collection("data").findOneAndUpdate(
+          if (actions.length > 0) {
+            await db.collection("data").updateOne(
               { username: req.user.username },
               {
-                $set: {
-                  snapshot: {
-                    version,
-                    snapshot,
-                    sequence: snapshotSequence
-                  }
+                $push: {
+                  actions: { $each: actions.map(action => String(action)) }
                 }
               }
             );
-            storedSnapshotSequence = snapshotSequence;
-            storedSnapshotVersion = version;
           }
-        }
-        const baseAnswer = {
-          snapshotSequence:
-            version === storedSnapshotVersion ? storedSnapshotSequence : 0,
-          seed
-        };
 
-        if (startFrom < sequence) {
-          if (
-            !snapshot &&
-            version === storedSnapshotVersion &&
-            storedSnapshotSequence > startFrom &&
-            JSON.stringify(newActions).length > storedSnapshot.snapshot.length
-          ) {
-            res.json({
-              ...baseAnswer,
-              replayFrom: startFrom,
-              replayLog: newActions.slice(storedSnapshotSequence - startFrom),
-              snapshot: storedSnapshot.snapshot
-            });
+          const newActions = data.actions.concat(actions);
+
+          const sequence = actionCount;
+          let storedSnapshotSequence = storedSnapshot.sequence
+            ? parseInt(storedSnapshot.sequence)
+            : 0;
+          let storedSnapshotVersion = storedSnapshot.version
+            ? parseInt(storedSnapshot.version)
+            : undefined;
+          trace(storedSnapshotSequence);
+          trace(storedSnapshotVersion);
+          trace(sequence);
+          trace(newActions);
+          if (snapshot) {
+            info(`Trying to store snapshot`);
+            if (
+              (storedSnapshotVersion === undefined ||
+                version >= storedSnapshotVersion) &&
+              startFrom === sequence
+            ) {
+              info(`Storing snapshot`);
+              const snapshotSequence = sequence + actions.length;
+              await db.collection("data").findOneAndUpdate(
+                { username: req.user.username },
+                {
+                  $set: {
+                    snapshot: {
+                      version,
+                      snapshot,
+                      sequence: snapshotSequence
+                    }
+                  }
+                }
+              );
+              storedSnapshotSequence = snapshotSequence;
+              storedSnapshotVersion = version;
+            }
+          }
+          const baseAnswer = {
+            snapshotSequence:
+              version === storedSnapshotVersion ? storedSnapshotSequence : 0,
+            seed
+          };
+
+          if (startFrom < sequence) {
+            if (
+              !snapshot &&
+              version === storedSnapshotVersion &&
+              storedSnapshotSequence > startFrom &&
+              JSON.stringify(newActions).length > storedSnapshot.snapshot.length
+            ) {
+              res.json({
+                ...baseAnswer,
+                replayFrom: startFrom,
+                replayLog: newActions.slice(storedSnapshotSequence - startFrom),
+                snapshot: storedSnapshot.snapshot
+              });
+            } else {
+              res.json({
+                ...baseAnswer,
+                replayFrom: startFrom,
+                replayLog: newActions
+              });
+            }
           } else {
             res.json({
-              ...baseAnswer,
-              replayFrom: startFrom,
-              replayLog: newActions
+              ...baseAnswer
             });
           }
-        } else {
-          res.json({
-            ...baseAnswer
-          });
+          if (actions.length > 0) {
+            (clients[req.user.username] || []).forEach(
+              c => c.session !== req.headers["x-sync-session"] && c.ws.send("")
+            );
+          }
+          info(`New action log length: ${sequence + actions.length}`);
+        } catch (err) {
+          error(err);
+          res.status(500).end(err);
         }
-        if (actions.length > 0) {
-          (clients[req.user.username] || []).forEach(
-            c => c.session !== req.headers["x-sync-session"] && c.ws.send("")
-          );
-        }
-        info(`New action log length: ${sequence + actions.length}`);
-      } catch (err) {
-        error(err);
-        res.status(500).end(err);
-        return;
-      }
+      });
+
+      delete locks[req.user.username];
     }
   );
 
